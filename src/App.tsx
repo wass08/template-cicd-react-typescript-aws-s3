649bc60b1c4163eb02475942683ac263a406ca44
Wassim SAMAD <wass08@gmail.com>
2021-09-07 21:22:54 +0200

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Wawa Sensei tutorial
+          Welcome to Wawa Sensei tutorial Test #2
         </p>
         <a
           className="App-link"
